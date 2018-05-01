caae70684af6bce1a87ceaaaecb295fe58a5a242
Olga Morozova <morozova31@gmail.com>
2018-05-01 11:54:54 +0300

    правки пхп

--- rsl/send.php
+++ rsl/send.php
@@ -1,5 +1,5 @@
 <?
-if((isset($_POST['name'])&&$_POST['name']!="")&&(isset($_POST['phone'])&&$_POST['phone']!="")){ //Проверка отправилось ли наше поля name и не пустые ли они
+if((isset($_POST['name'])&&$_POST['name']!="")&&(isset($_POST['firstname'])&&$_POST['secondname']&&$_POST['lastname']!="")){ //Проверка отправилось ли наше поля name и не пустые ли они
         $to = 'morozova31@gmail.com'; //Почта получателя, через запятую можно указать сколько угодно адресов
         $subject = 'Тестирование по английскому'; //Загаловок сообщения
         $message = '
@@ -17,7 +17,7 @@ if((isset($_POST['name'])&&$_POST['name']!="")&&(isset($_POST['phone'])&&$_POST[
                     </body>
                 </html>'; //Текст нащего сообщения можно использовать HTML теги
         $headers  = "Content-type: text/html; charset=utf-8 \r\n"; //Кодировка письма
-        $headers .= "From: Отправитель <from@example.com>\r\n"; //Наименование и почта отправителя
+        $headers .= "From: Отправитель <morozova31@yandex.ru>\r\n"; //Наименование и почта отправителя
         mail($to, $subject, $message, $headers); //Отправка письма с помощью функции mail
 }
 ?>
