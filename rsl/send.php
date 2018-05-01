be429a79f6c0417499df5e9222b5a920a38add49
Olga Morozova <morozova31@gmail.com>
2018-05-01 11:51:32 +0300

    файл php

--- rsl/send.php
+++ rsl/send.php
@@ -1,15 +1,19 @@
 <?
 if((isset($_POST['name'])&&$_POST['name']!="")&&(isset($_POST['phone'])&&$_POST['phone']!="")){ //Проверка отправилось ли наше поля name и не пустые ли они
-        $to = 'mail@yandex.ru'; //Почта получателя, через запятую можно указать сколько угодно адресов
-        $subject = 'Обратный звонок'; //Загаловок сообщения
+        $to = 'morozova31@gmail.com'; //Почта получателя, через запятую можно указать сколько угодно адресов
+        $subject = 'Тестирование по английскому'; //Загаловок сообщения
         $message = '
                 <html>
                     <head>
                         <title>'.$subject.'</title>
                     </head>
                     <body>
-                        <p>Имя: '.$_POST['name'].'</p>
-                        <p>Телефон: '.$_POST['phone'].'</p>                        
+                        <p>First name: '.$_POST['firstname'].'</p>
+                        <p>Second name: '.$_POST['secondname'].'</p>
+                        <p>Last name: '.$_POST['lastname'].'</p>
+                        <p>Home phone number: '.$_POST['homephone'].'</p>   
+                        <p>Office phone number: '.$_POST['officephone'].'</p>
+                        <p>Notes: '.$_POST['notes'].'</p>
                     </body>
                 </html>'; //Текст нащего сообщения можно использовать HTML теги
         $headers  = "Content-type: text/html; charset=utf-8 \r\n"; //Кодировка письма
