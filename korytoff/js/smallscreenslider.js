f2d5e6cdd4789b8ecf25692bc506de98135adef9
Olga Morozova <morozova31@gmail.com>
2019-02-02 17:01:20 +0300

    удалила ненужное
    1

--- korytoff/js/smallscreenslider.js
+++
@@ -1,9 +0,0 @@
-    $(document).ready(function () {
-        $("#lightslider2").lightSlider({
-            item: 1,
-            loop: true,
-            pager: false,
-            autoWidth: true,
-            adaptiveHeight: true
-        });
-    });
